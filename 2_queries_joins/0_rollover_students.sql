4e421b5d1ffc499d73d49a94d8ccca12cbe9fc1b
Jonathan Su <jsu6898@gmail.com>
2021-11-23 22:46:16 -0800

    reevaluated naming conventions & added 4_queries

---
+++ 2_queries_joins/0_rollover_students.sql
@@ -0,0 +1,5 @@
+SELECT students.name as student, cohorts.name as cohort, cohorts.start_date as cohort_start_date, students.start_date as student_start_date
+FROM students
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.start_date != students.start_date
+ORDER BY cohort_start_date;
