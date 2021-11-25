348059f3289c02d3f92c75c223b1a6ed3db815d5
Jonathan Su <jsu6898@gmail.com>
2021-11-25 06:51:03 -0800

    refactored js to prevent sql injection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -13,14 +13,17 @@ pool.connect(() => {
   console.log('Connected to the database');
 });
 
-
-pool.query(`SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+const queryString = `SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teachers.id = teacher_id
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE'%${process.argv[2] || 'JUL02'}%'
-ORDER BY teachers.name;`)
+WHERE cohorts.name LIKE $1
+ORDER BY teachers.name;`;
+
+const values = [`%${process.argv[2] || 'JUL02'}%`];
+
+pool.query(queryString, values)
   .then(res => {
     res.rows.forEach(fn => {
       console.log(`${fn.teacher}: ${fn.cohort}`);
